23754898847816f156a89ec356782155f7be561f
Md Rakibul Hasan <rakibhasan0308@gmail.com>
2023-01-07 12:24:22 +0530

    Complete full projects

--- Day 05/projects.py
+++ Day 05/projects.py
@@ -5,21 +5,42 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_letters = int(input("How many letters would you like in your password?\n")) 
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
-password = '';
-for pass_letter in range(nr_letters):
-    password += random.choice(letters)
-# print((password))
 
-for pass_numbers in range(nr_numbers):
-    password += random.choice((numbers))
+#Eazy Level
+# password = ""
+
+# for char in range(1, nr_letters + 1):
+#   password += random.choice(letters)
+
+# for char in range(1, nr_symbols + 1):
+#   password += random.choice(symbols)
+
+# for char in range(1, nr_numbers + 1):
+#   password += random.choice(numbers)
+
 # print(password)
 
-for pass_symbols in range(nr_symbols):
-    password += random.choice(symbols)
+#Hard Level
+password_list = []
 
-result = "".join(random.sample(password, len(password)))
-print(result)
-    
+for char in range(1, nr_letters + 1):
+  password_list.append(random.choice(letters))
+
+for char in range(1, nr_symbols + 1):
+  password_list += random.choice(symbols)
+
+for char in range(1, nr_numbers + 1):
+  password_list += random.choice(numbers)
+
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
+
+password = ""
+for char in password_list:
+  password += char
+
+print(f"Your password is: {password}")
