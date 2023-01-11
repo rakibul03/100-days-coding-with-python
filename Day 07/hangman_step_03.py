c83155c59b9bddfdf3d39a336c643338d42b4a53
Md Rakibul Hasan <rakibhasan0308@gmail.com>
2023-01-11 22:00:03 +0530

    setp 3 completed with full word check

--- Day 07/hangman_step_03.py
+++ Day 07/hangman_step_03.py
@@ -1,3 +1,5 @@
+#Step 3
+
 import random
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
@@ -11,15 +13,22 @@ display = []
 for _ in range(word_length):
     display += "_"
 
-guess = input("Guess a letter: ").lower()
+#TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
+end_of_game = False
 
-#Check guessed letter
-for position in range(word_length):
-    letter = chosen_word[position]
-    print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
-    if letter == guess:
-        display[position] = letter
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
 
+    #Check guessed letter
+    for position in range(word_length):
+        letter = chosen_word[position]
+        #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
 
+    print(display)
 
-print(display)
+    #Check if there are no more "_" left in 'display'. Then all letters have been guessed.
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
