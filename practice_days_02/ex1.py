86c2221d602d00be6864c6fcbc2c1453835d337f
Md Rakibul Hasan <rakibhasan0308@gmail.com>
2023-01-13 21:41:12 +0530

    pratice day two

---
+++ practice_days_02/ex1.py
@@ -0,0 +1,13 @@
+def prime(number):
+    is_prime = True
+    for i in range(2, number):
+        if number % i == 0:
+            is_prime = False
+
+    if is_prime:
+        print("It's a prime number.")
+    else:
+        print("It's not a prime number.")
+
+n = int(input("Enter e number: "))
+prime(number = n)
